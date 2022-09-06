2b2796ccfa0304377f2f46c30ac4bfe4774bc4cd
shinobiseb <sebpatin@gmail.com>
2022-09-06 18:03:03 -0400

    Form component started

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-        <h1 className='text-2xl mt-3'>List Conductor</h1>
+    <div className='w-full items-center flex flex-row justify-center p-3'>
+        <h1 className='text-2xl'>List Conductor</h1>
     </div>
   )
 }
--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,18 +1,43 @@
 import React from 'react'
-import { Track } from '../assets/tracks'
+import { Track, tracks } from '../assets/tracks'
 
 type SongProps = {
   track : Track;
 }
 
+//Minutes Calculator
+const minCalc= (duration : number) => {
+  const min = Math.floor(duration/60000)
+  return min
+}
+
+//Seconds Calculator 
+const secCalc= (duration : number) => {
+  const sec = Math.floor(duration/1000) % 60
+  if (sec < 10) {
+    return `0${sec}`
+  } else {
+    return sec
+  }
+}
+
 export default function Song({ track } : SongProps){
 
   return (
-    <div className='song-container flex flex-col mt-4 bg-light-blue rounded-md p-2'>
+    <div className='song-container flex flex-row justify-between mt-1 bg-light-blue rounded-md p-2'>
+
+      {/* Song Title and Artist */}
+
+      <div className="song-info">
         <h1 className='text-lg'> {track.title} </h1>
-        <h2> {track.artist} </h2>
-        <h1> {track.duration} </h1>
-        <h1> {track.link} </h1>
+        <h2 className='italic text-sm'> {track.artist} </h2>
+      </div>
+
+      {/* Song Stats and Numbers */}
+
+      <div className="song-stats flex items-center">
+        <h2> {minCalc(track.duration)}:{secCalc(track.duration)}</h2>
+      </div>
     </div>
   )
 }
