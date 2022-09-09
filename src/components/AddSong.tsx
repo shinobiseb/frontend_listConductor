fae133a4df95329478582c06a79fd628d8297c1c
shinobiseb <sebpatin@gmail.com>
2022-09-09 13:13:37 -0400

    onChange added

--- src/components/AddSong.tsx
+++ src/components/AddSong.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
 
-const [title, setTitle] = useState("test")
-
 export default function AddSong() {
+
+    const [title, setTitle] = useState("")
+
+    const [artist, setArtist] = useState("")
+
+    const handleSubmit = (event : any) => {
+    event.preventDefault()
+    console.log(`Title: ${title} Artist: ${artist}`)
+}
+
   return (
     <div className='flex flex-row w-3/4 justify-between'>
         <input 
         id="text-box-handle" className="rounded-lg p-2" 
         type="text" 
         placeholder='Title'
-        onChange={event => 
+        value={title}
+        onChange={event => {
             setTitle(event.target.value)
+            console.log(title)
+            }
         }
         />
 
@@ -18,11 +29,15 @@ export default function AddSong() {
         className="rounded-lg p-2"
         type="text"  
         placeholder='Artist'
+        value={artist}
+        onChange={event => {
+            setArtist(event.target.value)
+            console.log(artist)
+            }
+        }
         />
-
         <button 
         className='button rounded-lg bg-light-blue p-2 hover:ease-in duration-250'
-        
         > 
         Add New Song 
         </button>
