bd4aaeb7a7dc565c1cf06897b52ff7001f3c6ee7
shinobiseb <sebpatin@gmail.com>
2022-09-15 11:26:24 -0400

    Song State Created

--- src/components/AddSong.tsx
+++ src/components/AddSong.tsx
@@ -2,14 +2,22 @@ import React, { useState, useRef} from 'react'
 
 export default function AddSong() {
 
-  const [title, setTitle] = useState("")
-
-  const [artist, setArtist] = useState("")
+  const [currentSong, setSong] = useState({
+      artist: "",
+      title: "",
+      duration: 0,
+      link: "",
+      info: "",
+      isAgeRestricted: false,
+  })
 
   const titleInput = useRef(null)
-
   const artistInput = useRef(null)
+  const durationInput = useRef(null)
+  const linkInput = useRef(null)
 
+
+  //type narrowing => catch html element errors
   const isInput = (ele : any) => {
     if(!ele.current) {
       console.warn(`Missing ${ele} element!`);
@@ -19,23 +27,16 @@ export default function AddSong() {
     if (Element instanceof HTMLInputElement === false) {
       console.warn(`Got the wrong Element for ${ele}!`)
       return
-    } else {
-      console.log(ele.current.value)
     }
   }
 
 
   return (
-    <div className='flex flex-row w-3/4 justify-between'>
+    <div className='flex flex-col w-3/4 justify-between'>
         <input 
         id="text-box-handle" className="rounded-lg p-2" 
         type="text" 
         placeholder='Title'
-        value={title}
-        onChange={event => {
-            setTitle(event.target.value)
-            }
-        }
         ref={titleInput}
         />
 
@@ -43,18 +44,29 @@ export default function AddSong() {
         className="rounded-lg p-2"
         type="text"  
         placeholder='Artist'
-        value={artist}
-        onChange={event => {
-            setArtist(event.target.value)
-            }
-        }
         ref={artistInput}
         />
+
+        <input
+        className="rounded-lg p-2"
+        type="number"  
+        placeholder='duration'
+        ref={durationInput}
+        />
+
+        <input 
+        className="rounded-lg p-2"
+        type="text"  
+        placeholder='Link'
+        ref={linkInput}
+        />
+
         <button 
         className='button rounded-lg bg-light-blue p-2 hover:ease-in duration-250'
         onClick={() => {
           isInput(titleInput)
           isInput(artistInput)
+          isInput(durationInput)
         }}
         > 
         Add New Song
