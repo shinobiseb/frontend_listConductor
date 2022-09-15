cb7a4dcda6f03eb28b322bbca8ef3e1ba980b7fe
shinobiseb <sebpatin@gmail.com>
2022-09-15 16:30:46 -0400

    finished adding fields and input refs for each field

--- src/components/AddSong.tsx
+++ src/components/AddSong.tsx
@@ -19,10 +19,22 @@ export default function AddSong() {
       isAgeRestricted: false,
   })
 
+  //Input Checkers
   const titleInput = useRef(null)
   const artistInput = useRef(null)
   const durationInput = useRef(null)
   const linkInput = useRef(null)
+  const likesInput = useRef(null)
+  const dislikesInput = useRef(null)
+  const viewsInput = useRef(null)
+  const uploadedInput = useRef(null)
+  const ageInput = useRef(null)
+
+  const [age, setAge] = useState("false")
+
+  const handleChange = (event : any) => {
+    setAge(event.target.value)
+  }
 
 
   //type narrowing => catch html element errors
@@ -64,10 +76,54 @@ export default function AddSong() {
 
         <input 
         className="rounded-lg p-2"
-        type="text"  
-        placeholder='Link'
+        type="url"  
+        placeholder="https://youtube.com/example"
         ref={linkInput}
         />
+
+        <input 
+        className="rounded-lg p-2"
+        type="number"  
+        placeholder='likes'
+        ref={likesInput}
+        />
+
+        <input 
+        className="rounded-lg p-2"
+        type="number"  
+        placeholder='dislikes'
+        ref={dislikesInput}
+        />
+
+        <input 
+        className="rounded-lg p-2"
+        type="number"  
+        placeholder='views'
+        ref={viewsInput}
+        />
+
+        <input 
+        className="rounded-lg p-2"
+        type="date"  
+        placeholder='upload Dated'
+        ref={uploadedInput}
+        />
+        
+        <div>
+          <label> Age Restricted?</label>
+          <select 
+          name="ageRestricted" 
+          id="ageRestricted" 
+          required 
+          value={age} 
+          onChange={handleChange} 
+          ref={ageInput}
+          className="rounded-md ml-2"
+          >
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+        </div>
 
         <button 
         className='button rounded-lg bg-light-blue p-2 hover:ease-in duration-250'
@@ -76,6 +132,10 @@ export default function AddSong() {
           isInput(artistInput)
           isInput(durationInput)
           isInput(linkInput)
+          isInput(likesInput)
+          isInput(dislikesInput)
+          isInput(viewsInput)
+          console.log(viewsInput)
         }}
         > 
         Add New Song
