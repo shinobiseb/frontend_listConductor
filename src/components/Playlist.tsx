3900efa9e7ac6238189fdc48e7c322329f8237f9
shinobiseb <sebpatin@gmail.com>
2022-10-04 12:43:39 -0400

    trying to make playlist comp

---
+++ src/components/Playlist.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Tracklist } from '../assets/tracks'
+
+type playlistListProps = {
+    playlistList : Tracklist
+}
+
+export default function Playlist() {
+  return (
+    <div>Playlist</div>
+  )
+}
--- src/components/PlaylistList.tsx
+++ src/components/PlaylistList.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Tracklist } from "../assets/tracks"
+import Playlist from './Playlist';
+
 
 type playlistListProps = {
     playlistList : Tracklist
 }
 
+const trackMapper = ({ playlistList }: playlistListProps) => {
+  let num = -1;
+  const forE = playlistList.map((playlist) => {
+    num = num + 1;
+    return <Playlist key={num} playlist={playlist} />;
+  });
+  return forE;
+};
+
 export default function PlaylistList({ playlistList }: playlistListProps) {
   return (
     <div>
-        
+      
     </div>
 
     
