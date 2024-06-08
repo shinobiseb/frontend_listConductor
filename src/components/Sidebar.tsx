d592a15990bba6fe9ff53b36fb92cd809773be80
shinobiseb <sebpatin@gmail.com>
2024-06-08 16:02:57 -0400

    change to semantic HTML

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,11 +9,11 @@ export default function Sidebar({ userPlaylists, updatePlay }: sidebarReq) {
       <main className='sm:h-full w-full sm:w-1/3 bg-gray rounded-md p-4 text-white overflow-hidden'>
         <h1 className='sm:text-3xl font-semibold'>ListConductor</h1>
         <h3 className='sm:text-lg font-semibold my-7'>UserName</h3>
-        <ul className='flex flex-col justify-evenly sm:h-64 font-semibold mb-10'>
+        <nav className='flex flex-col justify-evenly sm:h-64 font-semibold mb-10'>
           <a className='hover:text-gunmetal' href='#'>HOME</a>
           <a className='hover:text-gunmetal' href='#'>PLAYLISTS</a>
           <a className='hover:text-gunmetal' href='#'>CONTACT</a>
-        </ul>
+        </nav>
           <PlaylistCollection playlistCollection={userPlaylists}/>
           <AddPlaylist addPlaylistToCollection={updatePlay}/>
       </main>
