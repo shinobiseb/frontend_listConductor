e097d5643e8e36fb7a7b5c299a30941cae3e073c
shinobiseb <sebpatin@gmail.com>
2024-06-30 03:08:36 -0400

    base change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend_listConductor",
+  base: "https://shinobiseb.github.io/frontend_listConductor/",
   plugins: [react()]
 })
