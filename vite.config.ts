66e3944d25e6da72926ff7b3ccbea4807d0ac619
shinobiseb <sebpatin@gmail.com>
2024-06-30 12:29:32 -0400

    updated no dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "https://shinobiseb.github.io/frontend_listConductor/",
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
