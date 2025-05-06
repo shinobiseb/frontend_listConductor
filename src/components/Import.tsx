9432e2f807550e8cbdb6e8922afdc8895e1d7bf4
shinobiseb <sebpatin@gmail.com>
2025-05-06 10:56:04 -0400

    songs import to spotify

--- src/components/Import.tsx
+++ src/components/Import.tsx
@@ -4,11 +4,44 @@ import { importPlaylistToSpotifyProps } from '../assets/types'
 
 export default function Import( {playlistToImport, userId} : importPlaylistToSpotifyProps ) {
 
+    async function addTracksToCreatedPlaylist( createdPlaylistID: string){
+        if(createdPlaylistID === ""){
+            console.error("No Created Playlist")
+        }
+
+        let playlistParams = {
+            "method": "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("access_token")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "uris": playlistURIMapper(),
+                "position": 0
+            })
+        }
+
+        try {
+            const response = await fetch(`https://api.spotify.com/v1/playlists/${createdPlaylistID}/tracks`, playlistParams);
+            if(!response.ok){
+                console.error("Response Error in Adding Tracks to Playlist: ", response)
+            }
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     async function createPlaylistInSpotify(){
         console.log("Starting Playlist Import Process")
 
         let playlistParams = {
             "method": "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("access_token")}`,
+                "Content-Type": "application/json"
+            },
             body: JSON.stringify({
                 "name" : playlistToImport.name,
                 "public" : true,
@@ -17,22 +50,38 @@ export default function Import( {playlistToImport, userId} : importPlaylistToSpo
             })
         }
 
+        if(!userId || userId === ""){
+            console.error("User ID: ", userId)
+            return
+        }
+
         try {
-            let response = await fetch(`/users/${userId}/playlists`, playlistParams)
+            let response = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, playlistParams)
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             let data = await response.json()
-            console.log(data)
+            addTracksToCreatedPlaylist(data.id)
         } catch (error) {
             console.error(error)
         }
         
     }
 
+    function playlistURIMapper() : string[] {
+        let tracksToImport = []
+
+        for(let i = 0; i < playlistToImport.tracks.length; i++){
+            tracksToImport.push(playlistToImport.tracks[i].uri)
+        }
+        return tracksToImport
+    }
+
     return (
         <main className='w-full flex justify-center items-center'>
-            <button className='button w-1/2 justify-center'>
+            <button 
+            onClick={createPlaylistInSpotify} 
+            className='button w-1/2 justify-center'>
                 Import Playlist to Spotify
             </button>
         </main>
